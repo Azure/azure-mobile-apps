ec183089930a3d35158a2f99c8710475fdae89d0
Adrian Hall <photoadrian@outlook.com>
2020-07-28 12:44:45 -0700

    (#94) Moved E2E server to tests

---
+++ dotnet/server/test/Azure.Mobile.Server.Test.E2EServer/Controllers/BlogCommentsController.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Mobile.Server.Entity;
+using Azure.Mobile.Server.Test.E2EServer.Database;
+using Azure.Mobile.Server.Test.E2EServer.DataObjects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure.Mobile.Server.Test.E2EServer.Controllers
+{
+    [Route("tables/blog_comments")]
+    [ApiController]
+    public class BlogCommentsController : TableController<BlogComment>
+    {
+        public BlogCommentsController(E2EDbContext context)
+        {
+            TableRepository = new EntityTableRepository<BlogComment>(context);
+        }
+    }
+}
---
+++ dotnet/server/test/Azure.Mobile.Server.Test.E2EServer/Controllers/UnitsController.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Mobile.Server.Entity;
+using Azure.Mobile.Server.Test.E2EServer.Database;
+using Azure.Mobile.Server.Test.E2EServer.DataObjects;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure.Mobile.Server.Test.E2EServer.Controllers
+{
+    [Route("tables/[controller]")]
+    [ApiController]
+    public class UnitsController : TableController<Unit>
+    {
+        public UnitsController(E2EDbContext context)
+        {
+            TableRepository = new EntityTableRepository<Unit>(context);
+        }
+    }
+}
