80b86c99e1dce687756c6775ef77617d37ba6fce
Adrian Hall <adhal@microsoft.com>
2020-07-29 15:01:14 -0700

    Update dotnet/server/src/Azure.Mobile.Server/TableController.cs
    Co-authored-by: Carlos J. Aliaga <carlos@cjaliaga.net>

--- dotnet/server/src/Azure.Mobile.Server/TableController.cs
+++ dotnet/server/src/Azure.Mobile.Server/TableController.cs
@@ -378,7 +378,7 @@ namespace Azure.Mobile.Server
 
             var entity = await TableRepository.LookupAsync(id).ConfigureAwait(false);
 
-            var operationValidation = await ValidateOperationAsync(TableOperation.Create, entity);
+            var operationValidation = await ValidateOperationAsync(TableOperation.Replace, entity);
             if (operationValidation != StatusCodes.Status200OK)
             {
                 return StatusCode(operationValidation);
