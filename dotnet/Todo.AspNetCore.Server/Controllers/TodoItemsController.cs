44fd50a8c19112daa749693a74ce32482f213929
Adrian Hall <photoadrian@outlook.com>
2020-07-30 14:11:03 -0700

    Added Todo Server sample

---
+++ dotnet/Todo.AspNetCore.Server/Controllers/TodoItemsController.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Mobile.Server;
+using Azure.Mobile.Server.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Todo.AspNetCore.Server.Database;
+
+namespace Todo.AspNetCore.Server.Controllers
+{
+    /// <summary>
+    /// API Controller for the /tables/todoitems endpoint, which is an Azure Mobile Apps
+    /// synchronizable table controller.
+    /// </summary>
+    [Route("tables/todoitems")]
+    [ApiController]
+    public class TodoItemsController : TableController<TodoItemDTO>
+    {
+        public TodoItemsController(AppDbContext context)
+        {
+            TableControllerOptions = new TableControllerOptions<TodoItemDTO>
+            {
+                DataView = item => item.OwnerId == HttpContext.User.Identity.Name,
+                MaxTop = 1000,
+                SoftDeleteEnabled = false
+            };
+            TableRepository = new EntityTableRepository<TodoItemDTO>(context);
+        }
+
+        /// <summary>
+        /// User is authorized for the operation if it's a list (controlled by the DataView above)
+        /// or a new record or the OwnerId matches the user id.
+        /// </summary>
+        /// <param name="operation">The operation being performed</param>
+        /// <param name="item">The item being resolved</param>
+        /// <returns></returns>
+        public override bool IsAuthorized(TableOperation operation, TodoItemDTO item)
+            => operation == TableOperation.List || operation == TableOperation.Create || item.OwnerId == HttpContext.User.Identity.Name;
+
+        /// <summary>
+        /// When we store data, we always tag the record with the users ownerId
+        /// </summary>
+        /// <param name="item">The item being stored</param>
+        /// <returns>The item to be stored</returns>
+        public override TodoItemDTO PrepareItemForStore(TodoItemDTO item)
+        {
+            item.OwnerId = HttpContext.User.Identity.Name;
+            return base.PrepareItemForStore(item);
+        }
+    }
+}
---
+++ dotnet/Todo.AspNetCore.Server/Database/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿namespace Todo.AspNetCore.Server.Database
+{
+    public static class DbInitializer
+    {
+        /// <summary>
+        /// Initializes the database within the context provided.
+        /// </summary>
+        /// <param name="context">The application database context</param>
+        public static void Initialize(AppDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+    }
+}
