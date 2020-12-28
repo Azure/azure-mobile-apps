a2041f89d9f978ac5897cdd5a32a5f2d2864f6e4
Adrian Hall <photoadrian@outlook.com>
2020-12-28 15:35:10 -0800

    Initial Xamrin.iOS changes

--- samples/xamarin-ios/SceneDelegate.cs
+++ samples/xamarin-ios/SceneDelegate.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace NewSingleViewTemplate {
 	[Register ("SceneDelegate")]
-	public class SceneDelegate : UIResponder, IUIWindowSceneDelegate {
-
+	public class SceneDelegate : UIResponder, IUIWindowSceneDelegate
+	{
 		[Export ("window")]
 		public UIWindow Window { get; set; }
 
