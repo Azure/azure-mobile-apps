c103a026d8f8db14b5ff4e86363fdcd8ae162937
Adrian Hall <photoadrian@outlook.com>
2021-01-18 20:06:17 -0700

    Added basic TodoListView

---
+++ samples/ios/ZumoQuickstart/ZumoQuickstart/Views/TodoListView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        VStack {
+            Text("Some text")
+        }
+    }
+}
+
+struct TodoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListView()
+    }
+}
