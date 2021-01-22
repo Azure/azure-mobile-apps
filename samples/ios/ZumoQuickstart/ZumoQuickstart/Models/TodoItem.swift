510b4d52b61799cc0d29141b41face9543e5b4e5
Adrian Hall <photoadrian@outlook.com>
2021-01-22 13:46:21 -0700

    Work in progress

--- samples/ios/ZumoQuickstart/ZumoQuickstart/Models/TodoItem.swift
+++ samples/ios/ZumoQuickstart/ZumoQuickstart/Models/TodoItem.swift
@@ -1,29 +1,35 @@
 import Foundation
 
-/**!
-    The definition of a single TodoItem.  This must match the TodoItem definition on the server.
+/**
+ The definition of a single TodoItem.  This must match the TodoItem definition on the server.
  */
 struct TodoItem: Codable, Identifiable {
-    /**!
-        The unique ID of this item.  This is generally a UUID, but can be anything as long as it is
-        guaranteed to be globally unique across all clients.
+    /**
+     The unique ID of this item.  This is generally a UUID, but can be anything as long as it is guaranteed to be globally unique across all clients.
      */
     let id: String
     
-    /**!
-        The text for the item.
+    /**
+     The text for the item.
      */
     var text: String
     
-    /**!
-        Whether the item is completed or not.
+    /**
+     Whether the item is completed or not.
      */
     var complete: Bool
     
-    /**!
-        Helper method to create a new TodoItem with some text.
+    /**
+     Helper method to create a new TodoItem with some text.
      */
     static func newTodoItem(_ text: String, complete: Bool = false) -> TodoItem {
         return TodoItem(id: UUID().uuidString, text: text, complete: complete)
+    }
+    
+    /**
+     Converts this item to a string for logging
+     */
+    func toString() -> String {
+        return "(\"\(id)\",\"\(text)\",\(complete))"
     }
 }
