9004f9a1a46274f91a9a884734ceb61705665d3e
Adrian Hall <photoadrian@outlook.com>
2021-06-16 16:56:22 -0700

    Added datasync client options

---
+++ sdk/dotnet/src/Microsoft.Datasync.Client/DatasyncClientOptions.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Microsoft.Datasync.Client
+{
+    /// <summary>
+    /// The options used to adjust the HTTP requests and responses for a datasync service.
+    /// </summary>
+    public class DatasyncClientOptions
+    {
+        private JsonSerializerOptions _deserializerOptions = new()
+        {
+            AllowTrailingCommas = true,
+            NumberHandling = JsonNumberHandling.Strict,
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            ReadCommentHandling = JsonCommentHandling.Skip
+        };
+
+        private IEnumerable<HttpMessageHandler> _httpPipeline = Array.Empty<HttpMessageHandler>();
+
+        private JsonSerializerOptions _serializerOptions = new()
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault,
+            NumberHandling = JsonNumberHandling.Strict,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+
+        private string _tablesUri = "tables";
+
+        /// <summary>
+        /// The serializer options (from System.Text.Json) used when deserializing content
+        /// from the datasync service.  This must match the serializer options on the service.
+        /// </summary>
+        public JsonSerializerOptions DeserializerOptions
+        {
+            get => _deserializerOptions;
+            set => _deserializerOptions = value ?? throw new ArgumentNullException(nameof(DeserializerOptions));
+        }
+
+        /// <summary>
+        /// A list of <see cref="DelegatingHandler"/> objects to be used as a pipeline for
+        /// requests.  The last entry can optionally be a <see cref="HttpClientHandler"/>.
+        /// </summary>
+        public IEnumerable<HttpMessageHandler> HttpPipeline
+        {
+            get => _httpPipeline;
+            set => _httpPipeline = value ?? throw new ArgumentNullException(nameof(HttpPipeline));
+        }
+
+        /// <summary>
+        /// The serializer options (from System.Text.Json) used when serializing content for
+        /// the datasync service.  This must match the deserializer options on the service.
+        /// </summary>
+        public JsonSerializerOptions SerializerOptions
+        {
+            get => _serializerOptions;
+            set => _serializerOptions = value ?? throw new ArgumentNullException(nameof(SerializerOptions));
+        }
+
+        /// <summary>
+        /// The relative Uri to the tables enpdoint.  This is used in constructing new tables endpoints.
+        /// </summary>
+        public string TablesUri
+        {
+            get => _tablesUri;
+            set => _tablesUri = (value ?? throw new ArgumentNullException(nameof(TablesUri))).Trim('/');
+        }
+    }
+}
---
+++ sdk/dotnet/test/Microsoft.Datasync.Client.Test/DatasyncClientOptions.Test.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Datasync.Client.Test.Helpers;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Microsoft.Datasync.Client.Test
+{
+    [ExcludeFromCodeCoverage(Justification = "Test suite")]
+    public class DatasyncClientOptions_Tests : BaseTest
+    {
+        #region Ctor
+        [Fact]
+        public void Ctor_ProvidesDefaults()
+        {
+            var options = new DatasyncClientOptions();
+
+            Assert.Equal(JsonNamingPolicy.CamelCase, options.DeserializerOptions.PropertyNamingPolicy);
+            Assert.Empty(options.HttpPipeline);
+            Assert.Equal(JsonNamingPolicy.CamelCase, options.SerializerOptions.PropertyNamingPolicy);
+            Assert.Equal(JsonIgnoreCondition.WhenWritingDefault, options.SerializerOptions.DefaultIgnoreCondition);
+            Assert.Equal("tables", options.TablesUri);
+        }
+        #endregion
+
+        #region DeserializerOptions
+        [Fact]
+        public void DeserializerOptions_Null_Throws()
+        {
+            var options = new DatasyncClientOptions();
+            Assert.Throws<ArgumentNullException>(() => options.DeserializerOptions = null);
+        }
+
+        [Fact]
+        public void DeserializerOptions_Roundtrips()
+        {
+            var options = new DatasyncClientOptions { DeserializerOptions = SerializerOptions };
+            Assert.Same(SerializerOptions, options.DeserializerOptions);
+        }
+        #endregion
+
+        #region HttpPipeline
+        [Fact]
+        public void HttpPipeline_Null_Throws()
+        {
+            var options = new DatasyncClientOptions();
+            Assert.Throws<ArgumentNullException>(() => options.HttpPipeline = null);
+        }
+
+        [Fact]
+        public void HttpPipeline_Roundtrips()
+        {
+            var handler = new HttpClientHandler();
+            var options = new DatasyncClientOptions { HttpPipeline = new HttpMessageHandler[] { handler } };
+            Assert.Single(options.HttpPipeline);
+            Assert.Same(handler, options.HttpPipeline.First());
+        }
+        #endregion
+
+        #region SerializerOptions
+        [Fact]
+        public void SerializerOptions_Null_Throws()
+        {
+            var options = new DatasyncClientOptions();
+            Assert.Throws<ArgumentNullException>(() => options.SerializerOptions = null);
+        }
+
+        [Fact]
+        public void SerializerOptions_Roundtrips()
+        {
+            var options = new DatasyncClientOptions { SerializerOptions = SerializerOptions };
+            Assert.Same(SerializerOptions, options.SerializerOptions);
+        }
+        #endregion
+
+        #region TablesUri
+        [Fact]
+        public void TablesUri_Null_Throws()
+        {
+            var options = new DatasyncClientOptions();
+            Assert.Throws<ArgumentNullException>(() => options.TablesUri = null);
+        }
+
+        [Theory]
+        [InlineData("api", "api")]
+        [InlineData("/api", "api")]
+        [InlineData("/api/", "api")]
+        [InlineData("api/", "api")]
+        public void TablesUri_RoundTrips(string sut, string expected)
+        {
+            var options = new DatasyncClientOptions { TablesUri = sut };
+            Assert.Equal(expected, options.TablesUri);
+        }
+        #endregion
+    }
+}
