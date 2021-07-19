3291f1f5241ef91a2132745fd56fa67648cc47ec
Adrian Hall <photoadrian@outlook.com>
2021-07-19 15:43:06 -0700

    Added LazyObservableCollection (need tests)

---
+++ sdk/dotnet/src/Microsoft.Datasync.Client/Commands/AsyncCommand.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Datasync.Client.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Microsoft.Datasync.Client.Commands
+{
+    /// <summary>
+    /// An implementation of the <see cref="IAsyncCommand"/> interface, used for
+    /// commands when doing lazy paged loading.
+    /// </summary>
+    public class AsyncCommand : IAsyncCommand
+    {
+        /// <summary>
+        /// An event handler that is triggered when <see cref="CanExecute"/> is changed.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        private bool _isExecuting;
+        private readonly Func<Task> _execute;
+        private readonly Func<bool> _canExecute;
+        private IAsyncExceptionHandler _handler;
+
+        /// <summary>
+        /// Creates a new <see cref="AsyncCommand"/>
+        /// </summary>
+        /// <param name="execute">The function to execute asynchronously</param>
+        /// <param name="canExecute">A boolean indicating whether it is ok to execute</param>
+        public AsyncCommand(Func<Task> execute, Func<bool> canExecute = null, IAsyncExceptionHandler handler = null)
+        {
+            Validate.IsNotNull(execute, nameof(execute));
+
+            _execute = execute;
+            _canExecute = canExecute;
+            _handler = handler;
+        }
+
+        /// <summary>
+        /// Determines if the execution can happen.
+        /// </summary>
+        /// <returns>true if the execution can happen</returns>
+        public bool CanExecute()
+            => !_isExecuting && (_canExecute?.Invoke() ?? true);
+
+        /// <summary>
+        /// Executes the task.
+        /// </summary>
+        public async Task ExecuteAsync()
+        {
+            if (CanExecute())
+            {
+                try
+                {
+                    _isExecuting = true;
+                    await _execute();
+                }
+                finally
+                {
+                    _isExecuting = false;
+                }
+            }
+
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        #region ICommand
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        bool ICommand.CanExecute(object parameter) => CanExecute();
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter"></param>
+        void ICommand.Execute(object parameter) => ExecuteAsync().FireAndForgetSafeAsync(_handler);
+        #endregion
+    }
+}
--- sdk/dotnet/src/Microsoft.Datasync.Client/Table/TableExtensions.cs
+++ sdk/dotnet/src/Microsoft.Datasync.Client/Table/TableExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
 // Licensed under the MIT License.
 
+using Microsoft.Datasync.Client.Commands;
+using Microsoft.Datasync.Client.Utils;
 using System.Collections.Generic;
 
 namespace Microsoft.Datasync.Client
@@ -21,12 +23,56 @@ namespace Microsoft.Datasync.Client
             => table.ToAsyncPageable();
 
         /// <summary>
+        /// Returns a <see cref="LazyObservableCollection{T}"/> for the table.
+        /// </summary>
+        /// <typeparam name="T">The type of entity being returned</typeparam>
+        /// <param name="table">The table to query</param>
+        /// <param name="pageCount">The number of items per page to return</param>
+        /// <param name="errorHandler">An <see cref="IAsyncExceptionHandler"/> for reporting errors.</param>
+        /// <returns>A <see cref="LazyObservableCollection{T}"/> representing all the items in the table.</returns>
+        public static LazyObservableCollection<T> ToLazyObservableCollection<T>(this IDatasyncTable<T> table, int pageCount, IAsyncExceptionHandler errorHandler = null)
+            => new InternalLazyObservableCollection<T>(table.ToAsyncEnumerable(), pageCount, errorHandler);
+
+        /// <summary>
+        /// Returns a <see cref="LazyObservableCollection{T}"/> for the table.
+        /// </summary>
+        /// <typeparam name="T">The type of entity being returned</typeparam>
+        /// <param name="table">The table to query</param>
+        /// <param name="errorHandler">An <see cref="IAsyncExceptionHandler"/> for reporting errors.</param>
+        /// <returns>A <see cref="LazyObservableCollection{T}"/> representing all the items in the table.</returns>
+
+        public static LazyObservableCollection<T> ToLazyObservableCollection<T>(this IDatasyncTable<T> table, IAsyncExceptionHandler errorHandler = null)
+            => new InternalLazyObservableCollection<T>(table.ToAsyncEnumerable(), errorHandler);
+
+        /// <summary>
         /// Returns an <see cref="IAsyncEnumerable{T}"/> for the query.
         /// </summary>
         /// <typeparam name="T">The type of entity being returned</typeparam>
-        /// <param name="table">The query to execute</param>
+        /// <param name="query">The query to execute</param>
         /// <returns>An <see cref="IAsyncEnumerable{T}"/> representing the items returned by executing the query.</returns>
-        public static IAsyncEnumerable<T> ToAsyncEnumerable<T>(this ITableQuery<T> table)
-            => table.ToAsyncPageable();
+        public static IAsyncEnumerable<T> ToAsyncEnumerable<T>(this ITableQuery<T> query)
+            => query.ToAsyncPageable();
+
+        /// <summary>
+        /// Returns a <see cref="LazyObservableCollection{T}"/> for the query.
+        /// </summary>
+        /// <typeparam name="T">The type of entity being returned</typeparam>
+        /// <param name="query">The query to execute</param>
+        /// <param name="pageCount">The number of items per page to return</param>
+        /// <param name="errorHandler">An <see cref="IAsyncExceptionHandler"/> for reporting errors.</param>
+        /// <returns>A <see cref="LazyObservableCollection{T}"/> representing all the items in the table.</returns>
+        public static LazyObservableCollection<T> ToLazyObservableCollection<T>(this ITableQuery<T> query, int pageCount, IAsyncExceptionHandler errorHandler = null)
+            => new InternalLazyObservableCollection<T>(query.ToAsyncEnumerable(), pageCount, errorHandler);
+
+        /// <summary>
+        /// Returns a <see cref="LazyObservableCollection{T}"/> for the query.
+        /// </summary>
+        /// <typeparam name="T">The type of entity being returned</typeparam>
+        /// <param name="query">The query to execute</param>
+        /// <param name="errorHandler">An <see cref="IAsyncExceptionHandler"/> for reporting errors.</param>
+        /// <returns>A <see cref="LazyObservableCollection{T}"/> representing all the items in the table.</returns>
+
+        public static LazyObservableCollection<T> ToLazyObservableCollection<T>(this ITableQuery<T> query, IAsyncExceptionHandler errorHandler = null)
+            => new InternalLazyObservableCollection<T>(query.ToAsyncEnumerable(), errorHandler);
     }
 }
