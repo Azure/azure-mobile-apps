a836271f33c0a9ff0e54496f9a7d68d1c1494d4d
Adrian Hall <adhal@microsoft.com>
2021-11-02 16:16:09 -0700

    New UWP project and shared view model (#239)

--- samples/TodoApp/TodoApp.Data/MVVM/IMVVMHelper.cs
+++ samples/TodoApp/TodoApp.Data/MVVM/IMVVMHelper.cs
@@ -3,28 +3,23 @@
 
 using System;
 using System.Threading.Tasks;
-using System.Windows;
-using TodoApp.Data.MVVM;
 
-namespace TodoApp.WPF
+namespace TodoApp.Data.MVVM
 {
     /// <summary>
-    /// Entry point for the application.
+    /// To properly do cross-platform MVVM, we sometimes need to indicate that code
+    /// is run on the UI thread.  This interface allows platform-specific implementations
+    /// of that.
     /// </summary>
-    public partial class App : Application, IMVVMHelper
+    public interface IMVVMHelper
     {
-        #region IMVVMHelper
         /// <summary>
         /// Runs the associated code on the UI thread so that the UI can
         /// be updated correctly.
         /// </summary>
         /// <param name="func">The code to be run</param>
         /// <returns>A task the completes when the code is completed.</returns>
-        public Task RunOnUiThreadAsync(Action func)
-        {
-            Dispatcher.Invoke(func);
-            return Task.CompletedTask;
-        }
+        Task RunOnUiThreadAsync(Action func);
 
         /// <summary>
         /// Displays a pop-up alert showing an error condition.
@@ -32,14 +27,6 @@ namespace TodoApp.WPF
         /// <param name="title">The title of the alert.</param>
         /// <param name="message">The content of the alert.</param>
         /// <returns>A task that completes when the user acknowledges the error.</returns>
-        public Task DisplayErrorAlertAsync(string title, string message)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
-            });
-            return Task.CompletedTask;
-        }
-        #endregion
+        Task DisplayErrorAlertAsync(string title, string message);
     }
 }
