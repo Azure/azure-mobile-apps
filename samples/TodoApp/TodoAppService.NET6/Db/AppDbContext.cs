71f8970378dcd4fa929e99f280dab01368f3d217
Adrian Hall <photoadrian@outlook.com>
2021-11-02 10:16:11 -0700

    First version of the NET6 service

---
+++ samples/TodoApp/TodoAppService.NET6/Db/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoAppService.NET6.Db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        /// <summary>
+        /// The dataset for the TodoItems.
+        /// </summary>
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+        /// <summary>
+        /// Do any database initialization required.
+        /// </summary>
+        /// <returns>A task that completes when the database is initialized</returns>
+        public async Task InitializeDatabaseAsync()
+        {
+            await this.Database.EnsureCreatedAsync().ConfigureAwait(false);
+        }
+    }
+}
