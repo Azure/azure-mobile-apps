95ff49c53c6cf1127d350ac1ffbcc7baca6b0ed6
Adrian Hall <photoadrian@outlook.com>
2022-03-05 15:30:23 -0800

    feat(offline): Added generic remote table integration test for delete operations.

---
+++ sdk/dotnet/test/Microsoft.Datasync.Integration.Test/Client/RemoteTableOfT/BaseOperationTest.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Datasync.Common.Test;
+using Datasync.Common.Test.Models;
+using Microsoft.Datasync.Client;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.Datasync.Integration.Test.Client.RemoteTableOfT
+{
+    [ExcludeFromCodeCoverage]
+    public abstract class BaseOperationTest : BaseTest
+    {
+        protected readonly DatasyncClient client;
+        protected readonly IRemoteTable<ClientMovie> soft, table;
+
+        protected BaseOperationTest()
+        {
+            client = GetMovieClient();
+            table = client.GetRemoteTable<ClientMovie>("movies");
+            soft = client.GetRemoteTable<ClientMovie>("soft");
+        }
+    }
+}
