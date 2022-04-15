062e98f9611072d86b614280f291f430b373c117
Adrian Hall <photoadrian@outlook.com>
2022-04-15 15:59:30 -0700

    Added TodoApp.iOS

---
+++ samples/TodoApp/TodoApp.iOS/DataSources/TodoItemsTableSource.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Foundation;
+using TodoApp.Data;
+using TodoApp.Data.Extensions;
+using TodoApp.Data.Models;
+using TodoApp.iOS.ViewControllers;
+using UIKit;
+
+namespace TodoApp.iOS.DataSources
+{
+    public class TodoItemsTableSource : UITableViewSource
+    {
+        private const string cellId = "todoitem";
+
+        public TodoItemsTableSource(UIViewController parent)
+        {
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// The parent view controller.
+        /// </summary>
+        protected UIViewController Parent { get; }
+
+        /// <summary>
+        /// The list of items being presented
+        /// </summary>
+        protected List<TodoItem> Items { get; } = new();
+
+        /// <summary>
+        /// Refresh the items in the list.
+        /// </summary>
+        /// <returns>A task that completes when the refresh is complete.</returns>
+        public void ReplaceAllItems(IEnumerable<TodoItem> items)
+        {
+            Items.Clear();
+            Items.AddRange(items);
+        }
+
+        /// <summary>
+        /// Inserts an item into the list.
+        /// </summary>
+        /// <param name="item">The item being inserted.</param>
+        /// <returns>The indexpath of the item inserted.</returns>
+        public NSIndexPath InsertItem(TodoItem item)
+        {
+            Items.Add(item);
+            var index = Items.FindIndex(x => x.Id == item.Id);
+            return NSIndexPath.FromItemSection(index, 0);
+        }
+
+        /// <summary>
+        /// Deletes an item into the list.
+        /// </summary>
+        /// <param name="item">The item being deleted.</param>
+        /// <returns>The indexpath of the item deleted.</returns>
+        public NSIndexPath DeleteItem(TodoItem item)
+        {
+            var index = Items.FindIndex(x => x.Id == item.Id);
+            Items.RemoveAt(index);
+            return NSIndexPath.FromItemSection(index, 0);
+        }
+
+        /// <summary>
+        /// Updates an item into the list.
+        /// </summary>
+        /// <param name="item">The item being updated.</param>
+        /// <returns>The indexpath of the item updated.</returns>
+        public NSIndexPath ReplaceItem(TodoItem item)
+        {
+            var index = Items.FindIndex(x => x.Id == item.Id);
+            Items[index] = item;
+            return NSIndexPath.FromItemSection(index, 0);
+        }
+
+        #region UITableViewSource
+        /// <summary>
+        /// Called by the <see cref="UITableView"/> to get the cell for a
+        /// specific index.
+        /// </summary>
+        /// <param name="tableView">The calling table view.</param>
+        /// <param name="indexPath">The index path of the item needed.</param>
+        /// <returns>The table view cell</returns>
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = (TodoItemTableViewCell)tableView.DequeueReusableCell(cellId) ?? new TodoItemTableViewCell(cellId);
+            cell.UpdateCell(Items[indexPath.Row]);
+            return cell;
+        }
+
+        /// <summary>
+        /// Called by the <see cref="UITableView"/> when a row is selected.
+        /// </summary>
+        /// <param name="tableView"></param>
+        /// <param name="indexPath"></param>
+        public override async void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            if (Parent is HomeViewController home)
+            {
+                await home.OnRowSelectedAsync(Items[indexPath.Row]);
+                tableView.DeselectRow(indexPath, true);
+            }
+        }
+
+        /// <summary>
+        /// Called by the <see cref="UITableView"/> to get the number of
+        /// sections.
+        /// </summary>
+        /// <param name="tableView">The calling table view.</param>
+        /// <returns>The number of sections.</returns>
+        public override nint NumberOfSections(UITableView tableView)
+            => 1;
+
+        /// <summary>
+        /// Called by the <see cref="UITableView"/> to get the number of
+        /// items in a specific section.
+        /// </summary>
+        /// <param name="tableview">The calling table view.</param>
+        /// <param name="section">The section being queried.</param>
+        /// <returns>The number of items in the section.</returns>
+        public override nint RowsInSection(UITableView tableview, nint section)
+            => Items.Count;
+        #endregion
+    }
+}
---
+++ samples/TodoApp/TodoApp.iOS/ViewControllers/HomeViewController.cs
@@ -0,0 +1,138 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using CoreGraphics;
+using System;
+using System.Threading.Tasks;
+using Foundation;
+using TodoApp.Data;
+using TodoApp.Data.Models;
+using TodoApp.iOS.DataSources;
+using UIKit;
+
+namespace TodoApp.iOS.ViewControllers
+{
+    public class HomeViewController : UIViewController
+    {
+        protected UIBarButtonItem addItemButton;
+        protected UITableView tableView;
+        protected TodoItemsTableSource tableSource;
+        protected ITodoService service = TodoService.Value;
+
+        public HomeViewController()
+        {
+            Title = "Todo Items";
+            service.TodoItemsUpdated += OnTodoItemsUpdated;
+        }
+
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Set background color
+            View.BackgroundColor = UIColor.White;
+
+            // Add Item button in nav bar
+            addItemButton = new UIBarButtonItem()
+            {
+                Image = UIImage.GetSystemImage("plus")
+            };
+            addItemButton.Clicked += async (sender, e) => await OnAddItemClicked();
+            NavigationItem.RightBarButtonItem = addItemButton;
+
+            // Add the table of items
+            tableSource = new TodoItemsTableSource(this);
+            tableView = new UITableView
+            {
+                Frame = new CGRect(0, 0, View.Bounds.Width, View.Bounds.Height),
+                RefreshControl = new UIRefreshControl(),
+                Source = tableSource
+            };
+            tableView.RefreshControl.ValueChanged += async (_,_) => await RefreshListAsync();
+            Add(tableView);
+
+            // Initialize the list
+            await RefreshListAsync();
+        }
+
+        /// <summary>
+        /// Refreshes the content of the list.
+        /// </summary>
+        public async Task RefreshListAsync()
+        {
+            tableView.RefreshControl?.BeginRefreshing();
+            await service.RefreshItemsAsync();
+            tableSource.ReplaceAllItems(await service.GetItemsAsync());
+            tableView.RefreshControl?.EndRefreshing();
+            tableView.ReloadData();
+        }
+
+        public void OnTodoItemsUpdated(object sender, TodoServiceEventArgs e)
+        {
+            NSIndexPath index;
+
+            switch (e.Action)
+            {
+                case TodoServiceEventArgs.ListAction.Add:
+                    index = tableSource.InsertItem(e.Item);
+                    tableView.InsertRows(new[] { index }, UITableViewRowAnimation.None);
+                    break;
+                case TodoServiceEventArgs.ListAction.Delete:
+                    index = tableSource.DeleteItem(e.Item);
+                    tableView.DeleteRows(new[] { index }, UITableViewRowAnimation.None);
+                    break;
+                case TodoServiceEventArgs.ListAction.Update:
+                    index = tableSource.ReplaceItem(e.Item);
+                    tableView.ReloadRows(new[] { index }, UITableViewRowAnimation.None);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Handles when a user selects a row.
+        /// </summary>
+        /// <param name="item">The item that was selected</param>
+        /// <returns>A task that completes when the operation is finished.</returns>
+        public async Task OnRowSelectedAsync(TodoItem item)
+        {
+            item.IsComplete = !item.IsComplete;
+            await service.SaveItemAsync(item);
+        }
+
+        /// <summary>
+        /// Event handler called when the Add Item button is clicked.
+        /// </summary>
+        protected async Task OnAddItemClicked()
+        {
+            var newItemTitle = await ShowAddItemDialogAsync();
+            var newItem = new TodoItem { Title = newItemTitle };
+            await service.SaveItemAsync(newItem);
+        }
+
+        /// <summary>
+        /// Displays the Add new item? dialog.
+        /// </summary>
+        /// <returns>A task that returns the added item text when complete.</returns>
+        protected static Task<string> ShowAddItemDialogAsync()
+        {
+            var tcs = new TaskCompletionSource<string>();
+            var alert = new UIAlertView
+            {
+                Title = "Add new item?",
+                AlertViewStyle = UIAlertViewStyle.PlainTextInput
+            };
+            nint okButtonIndex = alert.AddButton("OK");
+            nint cancelButtonIndex = alert.AddButton("Cancel");
+            alert.CancelButtonIndex = cancelButtonIndex;
+            alert.Clicked += (_, e) =>
+            {
+                if (e.ButtonIndex == okButtonIndex)
+                {
+                    tcs.TrySetResult(alert.GetTextField(0).Text);
+                }
+            };
+            alert.Show();
+            return tcs.Task;
+        }
+    }
+}
