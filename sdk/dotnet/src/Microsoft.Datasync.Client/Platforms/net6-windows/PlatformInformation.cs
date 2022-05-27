c211d4ab16b845c393cc122ea4b95779a4d9c64b
Adrian Hall <photoadrian@outlook.com>
2022-05-27 07:53:24 -0700

    Added .NET6 support

---
+++ sdk/dotnet/src/Microsoft.Datasync.Client/Platforms/net6-windows/PlatformInformation.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Datasync.Client.Utils;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Windows.ApplicationModel;
+
+namespace Microsoft.Datasync.Client.Platforms
+{
+    /// <summary>
+    /// UWP-specific implementation of the <see cref="IPlatformInformation"/> interface.
+    /// </summary>
+    internal class PlatformInformation : IPlatformInformation
+    {
+        /// <summary>
+        /// Information about the OS.
+        /// </summary>
+        public IOSInformation OS => new OSInformation
+        {
+            Architecture = Package.Current.Id.Architecture.ToString(),
+            Name = "Windows",
+            Version = System.Environment.OSVersion.VersionString
+        };
+
+        /// <summary>
+        /// The details section of the <c>User-Agent</c> header.
+        /// </summary>
+        public string UserAgentDetails
+        {
+            get => $"lang=dotnet6;os={OS.Name}/{OS.Version};arch={OS.Architecture};version={Platform.AssemblyVersion}";
+        }
+    }
+}
+
+
