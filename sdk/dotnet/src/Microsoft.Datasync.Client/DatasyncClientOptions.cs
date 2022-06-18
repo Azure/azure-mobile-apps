f8979c5cfb595ed2edbc6df52a7997d3dc0fe705
Adrian Hall <photoadrian@outlook.com>
2022-06-18 12:21:31 -0700

    (#359) Parallel queue runner

--- sdk/dotnet/src/Microsoft.Datasync.Client/DatasyncClientOptions.cs
+++ sdk/dotnet/src/Microsoft.Datasync.Client/DatasyncClientOptions.cs
@@ -15,6 +15,13 @@ namespace Microsoft.Datasync.Client
     /// </summary>
     public class DatasyncClientOptions
     {
+        private int _parallelOperations = 1;
+
+        /// <summary>
+        /// The maximum number of threads that can be requested.
+        /// </summary>
+        public const int MAX_PARALLEL_OPERATIONS = 8;
+
         /// <summary>
         /// The HTTP Pipeline to use.  This can be null.  If set, it must
         /// be an ordered set of <see cref="DelegatingHandler"/> objects,
@@ -34,6 +41,24 @@ namespace Microsoft.Datasync.Client
         /// The offline store to use for offline table storage.
         /// </summary>
         public IOfflineStore OfflineStore { get; set; }
+
+        /// <summary>
+        /// The number of parallel operations that can occur when running the 
+        /// push operation queue.  By default, we do this serially (i.e. one
+        /// thread).
+        /// </summary>
+        public int ParallelOperations
+        {
+            get => _parallelOperations;
+            set
+            {
+                if (value <= 0 || value > MAX_PARALLEL_OPERATIONS)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(ParallelOperations));
+                }
+                _parallelOperations = value;
+            }
+        }
 
         /// <summary>
         /// The serializer settings to use for this connection.
---
+++ sdk/dotnet/src/Microsoft.Datasync.Client/Offline/QueueHandler.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Datasync.Client.Offline.Queue;
+using System;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Microsoft.Datasync.Client.Offline
+{
+    /// <summary>
+    /// A parallel async queue runner that runs the table operations.
+    /// </summary>
+    internal class QueueHandler
+    {
+        /// <summary>
+        /// The jobs.
+        /// </summary>
+        private readonly ActionBlock<TableOperation> _jobs;
+
+        /// <summary>
+        /// Creates a new <see cref="QueueHandler"/>.
+        /// </summary>
+        /// <param name="maxThreads">The maximum number of threads to run.</param>
+        /// <param name="jobRunner">The job runner method.</param>
+        public QueueHandler(int maxThreads, Func<TableOperation, Task> jobRunner)
+        {
+            var options = new ExecutionDataflowBlockOptions()
+            {
+                MaxDegreeOfParallelism = maxThreads
+            };
+
+            _jobs = new ActionBlock<TableOperation>(jobRunner, options);
+        }
+
+        /// <summary>
+        /// Enqueue a new job.
+        /// </summary>
+        /// <param name="operation">The operation to be enqueued.</param>
+        public void Enqueue(TableOperation operation)
+        {
+            _jobs.Post(operation);
+        }
+
+        /// <summary>
+        /// Wait until all jobs have been completed.
+        /// </summary>
+        public Task WhenComplete()
+        {
+            _jobs.Complete();
+            return _jobs.Completion;
+        }
+
+        /// <summary>
+        /// The number of items still to be posted.
+        /// </summary>
+        public int Count { get => _jobs.InputCount; }
+    }
+}
