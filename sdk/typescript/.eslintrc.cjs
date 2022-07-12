38f34c56fd8d8fe523e5deb8602c16ee4604ba14
Adrian Hall <photoadrian@outlook.com>
2022-07-12 08:40:44 -0700

    Initial Checkin

---
+++ sdk/typescript/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json'
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ]
+};
---
+++ sdk/typescript/ms-datasync-client/src/datasyncClient.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export class DatasyncClient {
+    constructor(endpointUrl: string, authProvider: AuthenticationProvider, options: DatasyncClientOptions) {
+
+    }
+
+}
