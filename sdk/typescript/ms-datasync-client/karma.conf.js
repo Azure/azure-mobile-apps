6815024ad22d29b542c3dd0a719c7d2bac14b81f
Adrian Hall <photoadrian@outlook.com>
2022-07-15 16:15:51 -0700

    Added karma config file

---
+++ sdk/typescript/ms-datasync-client/karma.conf.js
@@ -0,0 +1,84 @@
+// Karma configuration
+// Generated on Fri Jul 15 2022 15:16:33 GMT-0700 (Pacific Daylight Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
+    frameworks: ['mocha', 'karma-typescript'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'src/**/*.ts',
+      'test/**/*.spec.ts'
+    ],
+
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
+    preprocessors: {
+      '**/*.ts': 'karma-typescript'
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
+    reporters: ['progress', 'karma-typescript'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+    // custom launch configurations for VSCode
+    customLaunchers: {
+      ChromeDebugging: {
+        base: 'Chrome',
+        flags: [ '--remote-debugging-port=9222' ]
+      }
+    },
+
+    // start these browsers
+    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    browsers: ['ChromeHeadless', 'ChromeDebugging'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser instances should be started simultaneously
+    concurrency: 1,
+
+    karmaTypescriptConfig: {
+      compilerOptions: {
+        esModuleInterop: true
+      }
+    }
+  });
+}
