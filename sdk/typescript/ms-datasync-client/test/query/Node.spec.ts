ad04754d361d4360213ee42fe56a31a3dcb9029d
Adrian Hall <photoadrian@outlook.com>
2022-07-25 13:35:03 -0700

    Added start of query expression visitor

---
+++ sdk/typescript/ms-datasync-client/test/query/Node.spec.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from 'chai';
+import { Node } from '../../src/query/Node';
+
+class Expression extends Node {
+    constructor() { super(); }
+}
+
+class BinaryExpression extends Expression {
+    constructor() { super(); }
+}
+
+describe('src/query/Node', () => {
+    describe('#constructor', () => {
+        it('sets type in constructor', () => {
+            const n = new Node();
+            assert.equal(n.type, 'Node');
+        });
+
+        it('sets type in derived constructor', () => {
+            const n = new Expression();
+            assert.equal(n.type, 'Expression');
+        });
+
+        it('sets type in derived heirarchy', () => {
+            const n = new BinaryExpression();
+            assert.equal(n.type, 'BinaryExpression');
+        });
+    });
+});
