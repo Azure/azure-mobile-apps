1b4d96dcf4d1265b2385dcd5049dc323939f8e80
Adrian Hall <photoadrian@outlook.com>
2022-12-12 13:53:37 -0800

    Removed TypeScript SDK

--- sdk/typescript/ms-datasync-client/src/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-/**
- * The protocol version used to communicate with the datasync service.
- */
-export const ApiProtocolVersion = "3.0.0";
--- sdk/typescript/ms-datasync-client/src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export * from "./errors";
-export * from "./policies";
-export * as validate from "./validate";
