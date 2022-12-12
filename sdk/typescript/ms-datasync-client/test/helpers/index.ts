af02a2a0e32b222bcf66ee1feb56589b025b3e2e
Adrian Hall <photoadrian@outlook.com>
2022-12-12 13:56:29 -0800

    Removed TypeScript SDK (#518)

--- sdk/typescript/ms-datasync-client/test/helpers/index.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
-
-export * as datasets from "./datasets";
-export * as models from "./models";
-export { MockHttpClient } from "./http-client";
