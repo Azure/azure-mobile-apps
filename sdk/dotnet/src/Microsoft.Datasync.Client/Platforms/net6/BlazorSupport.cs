4a7a686349339a8e1d19a76c0d0f4caac40e049b
Adrian Hall <photoadrian@outlook.com>
2022-12-21 11:40:32 -0800

    (#537) Blazor support for the InstallationId. (#539)
    * (#537) First pass - introduce the net6.0 target, which is a copy of netstandard2.0
    
    * (#537) Added check for WASM to specify an InstallationId.
    
    * (#537) Corrected last checkin.
    
    * (#537) Added Blazor Support class for generating the installation ID via the JSRuntime.
    
    * (#537) Updated SQLite library to latest

---
+++ sdk/dotnet/src/Microsoft.Datasync.Client/Platforms/net6/BlazorSupport.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.JSInterop;
+using System;
+
+namespace Microsoft.Datasync.Client
+{
+    public static class BlazorSupport
+    {
+        private const string installationIdLocalStorageKey = "ms-datasync-installation-id";
+
+        /// <summary>
+        /// Gets the installation ID in a JavaScript / Blazor WASM context.
+        /// </summary>
+        /// <param name="jsRuntime">The Javascript Runtime.</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">If the <see cref="IJSInProcessRuntime"/> is not available.</exception>
+        /// <exception cref="ApplicationException">If localstorage is not available.</exception>
+        public static string GetInstallationId(IJSRuntime jsRuntime)
+        {
+            if (jsRuntime is not IJSInProcessRuntime jsproc)
+            {
+                throw new InvalidOperationException("IJSInProcessRuntime is not available");
+            }
+            try
+            {
+                string installationId = jsproc.Invoke<string>("localStorage.getItem", installationIdLocalStorageKey);
+                if (installationId == null)
+                {
+                    installationId = Guid.NewGuid().ToString("D");
+                    jsproc.InvokeVoid("localStorage.setItem", installationIdLocalStorageKey, installationId);
+                }
+                return installationId;
+            }
+            catch (Exception exception)
+            {
+                if (exception.Message.Contains("Failed to read the 'localStorage' property from 'Window'"))
+                {
+                    throw new ApplicationException("Unable to access the browser storage. This is most likely due to the browser settings.");
+                }
+                throw;
+            }
+        }
+    }
+}
---
+++ sdk/dotnet/src/Microsoft.Datasync.Client/Platforms/net6/PlatformInformation.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Datasync.Client.Utils;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Microsoft.Datasync.Client.Platforms
+{
+    /// <summary>
+    /// Android-specific implementation of the <see cref="IPlatformInformation"/> interface.
+    /// </summary>
+    internal class PlatformInformation : IPlatformInformation
+    {
+        /// <summary>
+        /// Information about the OS.
+        /// </summary>
+        public IOSInformation OS => new OSInformation
+        {
+            Architecture = RuntimeInformation.OSArchitecture.ToString(),
+            Name = RuntimeInformation.OSDescription,
+            Version = System.Environment.OSVersion.VersionString
+        };
+
+        /// <summary>
+        /// Determines if the debugger is attached.
+        /// </summary>
+        /// <remarks>
+        /// Excluded from code coverage because the string.Empty version will never be returned in a test situation.
+        /// </remarks>
+        [ExcludeFromCodeCoverage]
+        private static string UnderTest => Debugger.IsAttached ? ";test" : string.Empty;
+
+        /// <summary>
+        /// The details section of the <c>User-Agent</c> header.
+        /// </summary>
+        public string UserAgentDetails
+        {
+            get => $"lang=Managed;os={OS.Name}/{OS.Version};arch={OS.Architecture};version={Platform.AssemblyVersion}{UnderTest}";
+        }
+    }
+}
+
