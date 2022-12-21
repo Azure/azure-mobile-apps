42823c110867a2139debea74437f43f58550eb4f
Adrian Hall <photoadrian@outlook.com>
2022-12-20 17:08:19 -0800

    (#537) First pass - introduce the net6.0 target, which is a copy of netstandard2.0

---
+++ sdk/dotnet/src/Microsoft.Datasync.Client/Platforms/net6/CurrentPlatform.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Datasync.Client.Utils;
+using System.IO.IsolatedStorage;
+using System;
+
+namespace Microsoft.Datasync.Client.Platforms
+{
+    /// <summary>
+    /// The .NET Standard-specific implementation of the <see cref="IPlatform"/> interface.
+    /// </summary>
+    internal class CurrentPlatform : IPlatform
+    {
+        /// <summary>
+        /// Lazy initializer for the <see cref="IApplicationStorage"/> implementation.
+        /// </summary>
+        private readonly Lazy<IApplicationStorage> _applicationStorage = new(() => CreateApplicationStorage());
+
+        /// <summary>
+        /// Accessor for the platform-specific <see cref="IApplicationStorage"/> implementation.
+        /// </summary>
+        public IApplicationStorage ApplicationStorage { get => _applicationStorage.Value; }
+
+        /// <summary>
+        /// Lazy initializer for the <see cref="IPlatformInformation"/> implementation.
+        /// </summary>
+        private readonly Lazy<IPlatformInformation> _platformInformation = new(() => new Platforms.PlatformInformation());
+
+        /// <summary>
+        /// Accessor for the platform-specific <see cref="IPlatformInformation"/> implementation.
+        /// </summary>
+        public IPlatformInformation PlatformInformation { get => _platformInformation.Value; }
+
+        private static IApplicationStorage CreateApplicationStorage()
+        {
+            return new Platforms.ApplicationStorage();
+        }
+    }
+}
