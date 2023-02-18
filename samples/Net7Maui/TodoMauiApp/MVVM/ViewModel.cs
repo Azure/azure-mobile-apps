14c1c020f618fd054bab518bb3ce5f287bf9b82a
Adrian Hall <photoadrian@outlook.com>
2023-02-17 16:41:56 -0800

    (#615) .NET 7 MAUI Sample

---
+++ samples/Net7Maui/TodoMauiApp/MVVM/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace TodoMauiApp.MVVM;
+
+public class ViewModel : INotifyPropertyChanged
+{
+    /// <summary>
+    /// The event handler required by <see cref="INotifyPropertyChanged"/>
+    /// </summary>
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    /// <summary>
+    /// Sets a backing store value and notify watchers of the change.  The type must
+    /// implement <see cref="IEquatable{T}"/> for proper comparisons.
+    /// </summary>
+    /// <typeparam name="T">The type of the value</typeparam>
+    /// <param name="storage">The backing store</param>
+    /// <param name="value">The new value</param>
+    /// <param name="propertyName"></param>
+    protected void SetProperty<T>(ref T storage, T value, string propertyName = null)
+    {
+        if (!storage.Equals(value))
+        {
+            storage = value;
+            NotifyPropertyChanged(propertyName);
+        }
+    }
+
+    /// <summary>
+    /// Notifies the data context that the property named has changed value.
+    /// </summary>
+    /// <param name="propertyName">The name of the property</param>
+    protected void NotifyPropertyChanged(string propertyName = null)
+    {
+        if (propertyName != null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ samples/Net7Maui/TodoMauiApp/Services/LoggingHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+
+namespace TodoMauiApp.Services;
+
+public class LoggingHandler : DelegatingHandler
+{
+    public LoggingHandler() : base()
+    {
+    }
+
+    public LoggingHandler(HttpMessageHandler innerHandler) : base(innerHandler)
+    {
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default)
+    {
+        Debug.WriteLine($"[HTTP] >>> {request.Method} {request.RequestUri}");
+        PrintHeaders(">>>", request.Headers);
+        await PrintContentAsync(">>>", request.Content);
+
+        HttpResponseMessage response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+
+        Debug.WriteLine($"[HTTP] <<< {response.StatusCode} {response.ReasonPhrase}");
+        PrintHeaders("<<<", response.Headers);
+        await PrintContentAsync("<<<", response.Content);
+
+        return response;
+    }
+
+    private static void PrintHeaders(string prefix, HttpHeaders headers)
+    {
+        foreach (var header in headers)
+        {
+            foreach (var hdrVal in header.Value)
+            {
+                Debug.WriteLine($"[HTTP] {prefix} {header.Key}: {hdrVal}");
+            }
+        }
+    }
+
+    private static async Task PrintContentAsync(string prefix, HttpContent content)
+    {
+        if (content != null)
+        {
+            PrintHeaders(prefix, content.Headers);
+            Debug.WriteLine($"[HTTP] {prefix} {await content.ReadAsStringAsync().ConfigureAwait(false)}");
+        }
+    }
+}
