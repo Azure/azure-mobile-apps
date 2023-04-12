995b2127f2a8b10c7d81761c3e1086e9dff89bae
Adrian Hall <photoadrian@outlook.com>
2023-04-12 10:53:53 -0700

    (release/6.0.0) project reset (part 2)

--- sdk/dotnet/test/Microsoft.AspNetCore.Datasync.EFCore.Test/Helpers/EFError.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
-// Licensed under the MIT License.
-
-namespace Microsoft.AspNetCore.Datasync.EFCore.Test;
-
-[ExcludeFromCodeCoverage]
-public class EFError : EntityTableData
-{
-}
--- sdk/dotnet/test/Microsoft.AspNetCore.Datasync.Test/Usings.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation. All Rights Reserved.
-// Licensed under the MIT License.
-
-global using Datasync.Common.Test;
-global using Moq;
-global using System.Diagnostics.CodeAnalysis;
-global using Xunit;
