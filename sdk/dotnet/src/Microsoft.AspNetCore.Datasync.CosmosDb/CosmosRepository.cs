72deac3d1aa4dacb3ed41a69c32a828f5f3a3023
Dylan Mills <dylan.mills@irisndt.com>
2023-10-12 20:33:20 -0600

    first draft, still need to parse IDs, partition keys and tests.

---
+++ sdk/dotnet/src/Microsoft.AspNetCore.Datasync.CosmosDb/CosmosRepository.cs
@@ -0,0 +1,226 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Datasync.CosmosDb
+{
+    /// <summary>
+    /// An implementation of an <see cref="IRepository{TEntity}"/> that stores
+    /// data in a Cosmos database.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of entity being stored.</typeparam>
+    public class CosmosRepository<TEntity> : IRepository<TEntity> where TEntity : CosmosTableData, ITableData
+    {
+        /// <summary>
+        /// Container where the <see cref="TEntity"/> is stored.
+        /// </summary>
+        private readonly Container container;
+
+        /// <summary>
+        /// Create a new <see cref="CosmosRepository{TEntity}"/> for accessing the database.
+        /// This is the normal ctor for this repository.
+        /// </summary>
+        /// <param name="cosmosClient">The <see cref="cosmosClient"/> for the backend store.</param>
+        /// <param name="databaseName">The name of the database.</param>
+        /// <param name="containerName">The name of the container.</param>
+        public CosmosRepository(CosmosClient cosmosClient, string databaseName, string containerName)
+        {
+            // Type check - only known derivates are allowed.
+            var typeInfo = typeof(TEntity);
+            if (!typeInfo.IsSubclassOf(typeof(CosmosTableData)))
+            {
+                throw new InvalidCastException($"Entity type {typeof(TEntity).Name} is not a valid entity type.");
+            }
+
+            ArgumentNullException.ThrowIfNull(cosmosClient, nameof(cosmosClient));
+            Database database;
+            try
+            {
+                database = cosmosClient.GetDatabase(databaseName);
+                _ = database.ReadAsync().ConfigureAwait(false);
+            }
+            catch (CosmosException cosmosException) when (cosmosException.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                throw new ArgumentException($"Cosmos database does not exist: {databaseName}", nameof(databaseName));
+            }
+
+            try
+            {
+                container = database.GetContainer(containerName);
+                _ = container.ReadContainerAsync().ConfigureAwait(false);
+            }
+            catch (CosmosException cosmosException) when (cosmosException.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                throw new ArgumentException($"Container does not exist in {databaseName}: {containerName}", nameof(containerName));
+            }
+        }
+
+        /// <summary>
+        /// Returns an unexecuted <see cref="IQueryable{T}"/> that represents the data store as a whole.
+        /// This is adjusted by the <see cref="TableController{TEntity}"/> to account for filtering and
+        /// paging requests.
+        /// </summary>
+        /// <returns>An <see cref="IQueryable{T}"/> for the entities in the data store.</returns>
+        public IQueryable<TEntity> AsQueryable() => container.GetItemLinqQueryable<TEntity>();
+
+        /// <summary>
+        /// Returns an unexecuted <see cref="IQueryable{T}"/> that represents the data store as a whole.
+        /// This is adjusted by the <see cref="TableController{TEntity}"/> to account for filtering and
+        /// paging requests.
+        /// </summary>
+        /// <returns>An <see cref="IQueryable{T}"/> for the entities in the data store.</returns>
+        public Task<IQueryable<TEntity>> AsQueryableAsync() => Task.FromResult(AsQueryable());
+
+        /// <summary>
+        /// Creates an entity within the data store. After completion, the system properties
+        /// within the entity have been updated with new values.
+        /// </summary>
+        /// <param name="entity">The entity to be created.</param>
+        /// <param name="token">A cancellation token.</param>
+        /// <exception cref="ConflictException">if the entity to be created already exists.</exception>
+        /// <exception cref="RepositoryException">if an error occurs in the data store.</exception>
+        public async Task CreateAsync(TEntity entity, CancellationToken token = default)
+        {
+            ArgumentNullException.ThrowIfNull(entity, nameof(entity));
+            ArgumentNullException.ThrowIfNull(entity.Id, nameof(entity.Id));
+
+            try
+            {
+                entity.Id ??= Guid.NewGuid().ToString("N");
+                entity.UpdatedAt = DateTimeOffset.UtcNow;
+                entity.EntityTag = entity.UpdatedAt.ToUnixTimeSeconds().ToString();
+                await container.CreateItemAsync(entity, cancellationToken: token);
+            }
+            catch (CosmosException cosmosException) when (cosmosException.StatusCode == System.Net.HttpStatusCode.Conflict)
+            {
+                //TODO need partition key to get existing
+                TEntity storeEntity = await container.ReadItemAsync<TEntity>(entity.Id, new Azure.Cosmos.PartitionKey(entity.Id));
+                throw new ConflictException(storeEntity);
+            }
+            catch (CosmosException cosmosException)
+            {
+                throw new RepositoryException(cosmosException.Message, cosmosException);
+            }
+        }
+
+        /// <summary>
+        /// Removes an entity from the data store. If a <c>version</c> is provided, the version
+        /// must match the entity version.
+        /// </summary>
+        /// <param name="id">The globally unique ID of the entity to be removed.</param>
+        /// <param name="version">The (optional) version of the entity to be removed.</param>
+        /// <param name="token">A cancellation token.</param>
+        /// <exception cref="NotFoundException">if the entity does not exist.</exception>
+        /// <exception cref="PreconditionFailedException">if the entity version does not match the provided version</exception>
+        /// <exception cref="RepositoryException">if an error occurs in the data store.</exception>
+        public async Task DeleteAsync(string id, byte[] version = null, CancellationToken token = default)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new BadRequestException();
+            }
+
+            try
+            {
+                //TODO need partition key to get existing
+                TEntity storeEntity = await container.ReadItemAsync<TEntity>(id, new Azure.Cosmos.PartitionKey(id), cancellationToken: token);
+                if (PreconditionFailed(version, storeEntity.Version))
+                {
+                    throw new PreconditionFailedException(storeEntity);
+                }
+                //TODO need partition key
+                await container.DeleteItemAsync<TEntity>(id, new Azure.Cosmos.PartitionKey(id), cancellationToken: token);
+            }
+            catch (CosmosException cosmosException)
+            {
+                throw new RepositoryException(cosmosException.Message, cosmosException);
+            }
+        }
+
+        /// <summary>
+        /// Reads the entity from the data store.
+        /// </summary>
+        /// <remarks>
+        /// </remarks>
+        /// <param name="id">The globally unique ID of the entity to be read.</param>
+        /// <param name="token">A cancellation token</param>
+        /// <returns>The entity, or null if the entity does not exist.</returns>
+        /// <exception cref="RepositoryException">if an error occurs in the data store.</exception>
+        public async Task<TEntity> ReadAsync(string id, CancellationToken token = default)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new BadRequestException();
+            }
+            try
+            {
+                //TODO need partition key to get existing
+                return await container.ReadItemAsync<TEntity>(id, new Azure.Cosmos.PartitionKey(id), cancellationToken: token);
+            }
+            catch (CosmosException cosmosException) when (cosmosException.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+            catch (CosmosException cosmosException)
+            {
+                throw new RepositoryException(cosmosException.Message, cosmosException);
+            }
+        }
+
+        /// <summary>
+        /// Replace the entity within the store with the provided entity.  If a <c>version</c> is
+        /// specified, then the version must match.  On return, the system properties of the entity
+        /// will be updated.
+        /// </summary>
+        /// <param name="entity">The replacement entity.</param>
+        /// <param name="version">The (optional) version of the entity to be replaced</param>
+        /// <param name="token">A cancellation token</param>
+        /// <exception cref="BadRequestException">if the entity does not have an ID</exception>
+        /// <exception cref="NotFoundException">if the entity does not exist</exception>
+        /// <exception cref="ConflictException">if the entity version does not match the provided version</exception>
+        /// <exception cref="RepositoryException">if an error occurs in the data store.</exception>
+        public async Task ReplaceAsync(TEntity entity, byte[] version = null, CancellationToken token = default)
+        {
+            ArgumentNullException.ThrowIfNull(entity, nameof(entity));
+            if (string.IsNullOrEmpty(entity.Id))
+            {
+                throw new BadRequestException();
+            }
+
+            try
+            {
+                //TODO need partition key to get existing
+                TEntity storeEntity = await container.ReadItemAsync<TEntity>(entity.Id, new Azure.Cosmos.PartitionKey(entity.Id), cancellationToken: token);
+                if (PreconditionFailed(version, storeEntity.Version))
+                {
+                    throw new PreconditionFailedException(storeEntity);
+                }
+                entity.UpdatedAt = DateTimeOffset.UtcNow;
+                entity.EntityTag = entity.UpdatedAt.ToUnixTimeSeconds().ToString();
+                // TODO do we have etag to check for here?
+                TEntity newEntity = await container.ReplaceItemAsync(entity, entity.Id, cancellationToken: token);
+                entity.UpdatedAt = newEntity.UpdatedAt;
+                entity.Version = newEntity.Version;
+            }
+            catch (CosmosException cosmosException)
+            {
+                throw new RepositoryException(cosmosException.Message, cosmosException);
+            }
+        }
+
+        /// <summary>
+        /// Checks that the version provided matches the version in the database.
+        /// </summary>
+        /// <param name="expectedVersion">The requested version.</param>
+        /// <param name="currentVersion">The current store version.</param>
+        /// <returns>True if we need to throw a <see cref="PreconditionFailedException"/>.</returns>
+        internal static bool PreconditionFailed(byte[] expectedVersion, byte[] currentVersion)
+           => expectedVersion != null && currentVersion?.SequenceEqual(expectedVersion) != true;
+    }
+}
---
+++ sdk/dotnet/src/Microsoft.AspNetCore.Datasync.CosmosDb/CosmosTableData.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.AspNetCore.Datasync.CosmosDb
+{
+    /// <summary>
+    /// An implementation of <see cref="ITableData"/> that is appropriate
+    /// for Cosmos databases that have an ETag (string-based) versioning
+    /// concurrency check instead of a byte[] based versioning concurrency
+    /// check
+    /// </summary>
+    public class CosmosTableData : ITableData
+    {
+        /// <summary>
+        /// The globally unique ID for this entity.
+        /// </summary>
+        [Key]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The date/time that the entity was updated.
+        /// </summary>
+        public DateTimeOffset UpdatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+        /// <summary>
+        /// The row version for the entity.
+        /// </summary>
+        [NotMapped]
+        public byte[] Version
+        {
+            get => Encoding.UTF8.GetBytes(EntityTag ?? string.Empty);
+            set { EntityTag = Encoding.UTF8.GetString(value); }
+        }
+
+        /// <summary>
+        /// The ETag for the entity.
+        /// </summary>
+        [Timestamp]
+        [JsonIgnore]
+        public string EntityTag { get; set; }
+
+        /// <summary>
+        /// True if the entity is marked as deleted.
+        /// </summary>
+        public bool Deleted { get; set; }
+
+        /// <summary>
+        /// Implements the <see cref="IEquatable{T}"/> interface to determine
+        /// if the system properties match the system properties of the other
+        /// entity.
+        /// </summary>
+        /// <param name="other">The other entity</param>
+        /// <returns>true if the entity matches and the system properties are set.</returns>
+        public bool Equals(ITableData other)
+            => other != null
+            && Id == other.Id
+            && UpdatedAt == other.UpdatedAt
+            && Deleted == other.Deleted
+            && Version.SequenceEqual(other.Version);
+    }
+}
