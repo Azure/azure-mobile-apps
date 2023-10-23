f5cff8a805a2f4548ee58367d9355661f206fd81
Adrian Hall <photoadrian@outlook.com>
2023-10-23 09:07:39 -0700

    (#799) Added option to send in an IEdmModel to the table controller, and cache generated IEdmModels.

---
+++ sdk/dotnet/src/Microsoft.AspNetCore.Datasync/Tables/ModelCache.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All Rights Reserved.
+// Licensed under the MIT License.
+
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using System;
+using System.Collections.Concurrent;
+
+namespace Microsoft.AspNetCore.Datasync.Tables;
+
+/// <summary>
+/// There is a memory issue if we create EdmModels for each request.  This class
+/// caches the <see cref="IEdmModel"/> needed for each table controller.
+/// </summary>
+internal static class ModelCache
+{
+    private static readonly Lazy<ConcurrentDictionary<Type, IEdmModel>> _cache = new(() => new ConcurrentDictionary<Type, IEdmModel>());
+
+    /// <summary>
+    /// Returns a cached version of the <see cref="IEdmModel"/> for a specific type.
+    /// </summary>
+    /// <param name="type">The type to reference.</param>
+    /// <returns>The <see cref="IEdmModel"/> for the type.</returns>
+    internal static IEdmModel GetEdmModel(Type type)
+    {
+        if (!_cache.Value.TryGetValue(type, out var model))
+        {
+            var modelBuilder = new ODataConventionModelBuilder();
+            modelBuilder.EnableLowerCamelCase();
+            modelBuilder.AddEntityType(type);
+            model = modelBuilder.GetEdmModel();
+            _cache.Value.TryAdd(type, model);
+        }
+        return model;
+    }
+}
