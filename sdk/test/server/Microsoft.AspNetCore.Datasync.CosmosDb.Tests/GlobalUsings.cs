5ad82132648e09734e59d9f4bc580e4a813fe520
Adrian Hall false <adhal@microsoft.com>
2023-11-07 10:17:49 -0800

    Removed the CosmosDB tests

--- sdk/test/server/Microsoft.AspNetCore.Datasync.CosmosDb.Tests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All Rights Reserved.
-// Licensed under the MIT License.
-
-global using Datasync.Common;
-global using FluentAssertions;
-global using System.Diagnostics.CodeAnalysis;
-global using Xunit;
-
-global using Datasync.Common.TestData;
